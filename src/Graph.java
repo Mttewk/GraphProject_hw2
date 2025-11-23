6ccda5d29c53ece893079c27a7d273b70fd97e0e
Mttewk <mttewk@gmail.com>
2025-11-23 23:52:26 +0300

    Использование ArrayList

--- src/Graph.java
+++ src/Graph.java
@@ -1,37 +1,36 @@
+import java.util.ArrayList;
+
 public class Graph {
-    private final String[] vertices = new String[100];
-    private final int[][] edges = new int[100][100];
-    private int vertexCount = 0;
+    private final ArrayList<String> vertices = new ArrayList<>();
+    private final ArrayList<ArrayList<String>> adj = new ArrayList<>();
 
     public void addVertex(String v) {
-        if (findVertex(v) == -1) {
-            vertices[vertexCount++] = v;
+        if (!vertices.contains(v)) {
+            vertices.add(v);
+            adj.add(new ArrayList<>());
         }
     }
 
     public void addEdge(String a, String b) {
-        int i = findVertex(a);
-        int j = findVertex(b);
-        if (i == -1 || j == -1) return;
-        edges[i][j] = 1;
-        edges[j][i] = 1;
+        addVertex(a);
+        addVertex(b);
+        int i = vertices.indexOf(a);
+        int j = vertices.indexOf(b);
+        if (!adj.get(i).contains(b)) {
+            adj.get(i).add(b);
+            adj.get(j).add(a);
+        }
     }
 
     public void print() {
-        for (int i = 0; i < vertexCount; i++) {
-            System.out.print(vertices[i] + " -> ");
-            for (int j = 0; j < vertexCount; j++) {
-                if (edges[i][j] == 1) {
-                    System.out.print(vertices[j] + " ");
-                }
-            }
-            System.out.println();
+        for (int i =0; i < vertices.size(); i++) {
+            System.out.println(vertices.get(i) + " -> " + adj.get(i));
         }
     }
 
     private int findVertex(String v) {
-        for (int i = 0; i < vertexCount; i++) {
-            if (vertices[i].equals(v)) return i;
+        for (int i = 0; i < vertices.size(); i++) {
+            if (vertices.get(i).equals(v)) return i;
         }
         return -1;
     }
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         Graph g = new Graph();
+
         g.addVertex("A");
         g.addVertex("B");
         g.addVertex("C");
